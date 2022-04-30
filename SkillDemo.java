6df7855cc1dbba77eb31d261f882f7f76c77645f
HaohanZou <pista5534@gmail.com>
2022-04-30 09:31:40 -0700

    Add method and tests

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,4 +1,7 @@
 public class SkillDemo{
-
+    public static int multiply(int a, int b){
+        int product = a*b;
+        return product;
+    }
 
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoTest{
+    // Here we need to add several tests to make sure method multiply can work properly
+
+    @Test
+    public void test1(){
+        assertEquals(3, SkillDemo.multiply(1,2)); //A test that fails
+    }
+
+
+}
